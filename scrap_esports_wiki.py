f8f852579efb5b4c409dd7b9a5281db12a0460e5
unknown <greg15001@gmail.com>
2015-10-04 01:54:19 -0400

    Added dict to scraper for gold and kills

--- scrap_esports_wiki.py
+++ scrap_esports_wiki.py
@@ -5,7 +5,8 @@ import requests
 
 # Need to get it to look like this:
 # {'color': 'blue', 'won': True, 'assists': 37, 'game_number': 1, 'team_name': 'H2K', 'total_gold': 51230,
-# 'team_id': 1273, 'game_id': 6092, 'deaths': 5, 'game_length_minutes': 27.816666666666666, 'kills': 16, 'minions_killed': 783}
+# 'team_id': 1273, 'game_id': 6092, 'deaths': 5, 'game_length_minutes': 27.816666666666666, 'kills': 16,
+# 'minions_killed': 783}
 
 
 
@@ -19,14 +20,44 @@ soup_tables = soup.find_all("table", {"class": "wikitable matchrecap1"})
 # soup_stuff = soup_tables.find_all("table", {"class": "wikitable matchrecap2"})
 # print(soup_stuff)
 all_td = []
+all_col = []
+blue_team = {'color': 'blue'}
+red_team = {'color': 'red'}
 for table in soup_tables:
     recap_tables = table.find_all("table", {"class": "wikitable matchrecap2"})
     for recap_table in recap_tables:
         for row in recap_table.find_all("tr"):
             cols = row.find_all('td')
             for col in cols:
+                if len(col.contents) == 3:
+                    if 'div' not in str(col.contents[1]):
+                        if col.span['title'] == 'Total Gold':
+                            if len(col.contents[0].strip()) == 0:
+                                total_gold = float(col.contents[2].string.strip().replace('k', '')) * 1000
+                                blue_team['total_gold'] = total_gold
+                            if len(col.contents[2].strip()) == 0:
+                                total_gold = float(col.contents[0].string.strip().replace('k', '')) * 1000
+                                red_team['total_gold'] = total_gold
+                        if col.span['title'] == 'Total Kills':
+                            if len(col.contents[0].strip()) == 0:
+                                kills = int(col.contents[2].string.strip())
+                                blue_team['kills'] = kills
+                            if len(col.contents[2].strip()) == 0:
+                                kills = int(col.contents[0].string.strip())
+                                red_team['kills'] = kills
                 all_td.append(str(col.contents[0]))
+                all_col.append(col)
     all_td = list(map(lambda x: x.strip(), all_td))
     all_td = list(filter(lambda x: x.strip(), all_td))
-    print(all_td)
+    # print(all_col)
+    blue_team['team_name'] = all_td[0]
+    red_team['team_name'] = all_td[3]
+    game_length_minutes = all_td[7].replace(':', '.')
+    blue_team['game_length_minutes'] = game_length_minutes
+    red_team['game_length_minutes'] = game_length_minutes
+    print(cols[1])
+    print(all_col[13])
+    print(all_col[14])
+    print(blue_team)
+    print(red_team)
     break
